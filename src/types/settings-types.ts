3036e7673d0a637d34248d30f8e3962561d821ad
Eli Veffer <eli@vef.dev>
2025-08-10 15:17:45 +0300

    update types

--- src/types/settings-types.ts
+++ src/types/settings-types.ts
@@ -1,10 +1,16 @@
+import { InField } from "@inspatial/cloud-client/types";
 import type { InValue } from "./field-types.ts";
 import type { BaseType, BaseTypeConfig } from "./shared-types.ts";
 
 export interface Settings {
   [key: string]: any;
 }
-
+export interface SettingsAction {
+  key: string;
+  label?: string;
+  description?: string;
+  params: Array<InField>;
+}
 export interface SettingsWithTimestamp<S extends Settings = Settings> {
   data: S;
   updatedAt: {
@@ -16,4 +22,5 @@ export interface SettingsTypeConfig extends BaseTypeConfig {
 
 export interface SettingsType extends BaseType {
   config: SettingsTypeConfig;
+  actions: Array<SettingsAction>;
 }
